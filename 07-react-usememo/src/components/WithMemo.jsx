fc69ab843c738d61ba22b6dcccfcbb4406808a6d
trianandafajar <mail.trianandafajar@gmail.com>
2025-05-13 10:51:30 +0700

    Update WIth Memo

--- 07-react-usememo/src/components/WithMemo.jsx
+++ 07-react-usememo/src/components/WithMemo.jsx
@@ -1,40 +1,45 @@
-import { useMemo, useState } from "react";
+import { useMemo, useState, useCallback } from "react";
 
 const expensiveCalculation = (num) => {
+  console.log("Calculating...");
   for (let i = 0; i < 1000000000; i++) {
     num += 1;
   }
   return num;
 };
+
 const WithMemo = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
+
   const calculation = useMemo(() => expensiveCalculation(count), [count]);
 
-  const increment = () => {
-    setCount((c) => c + 1);
-  };
+  const increment = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
 
-  const addTodo = () => {
-    setTodos((t) => [...t, "New Todo"]);
-  };
+  const addTodo = useCallback(() => {
+    setTodos((prev) => [...prev, "New Todo"]);
+  }, []);
 
   return (
-    <>
+    <div>
       <div>
         <h2>My Todos</h2>
-        {todos.map((todo, index) => {
-          return <p key={index}>{todo}</p>;
-        })}
+        {todos.map((todo, index) => (
+          <p key={index}>{todo}</p>
+        ))}
         <button onClick={addTodo}>Add Todo</button>
       </div>
-      <br />
+
+      <hr />
+
       <div>
-        Count : {count}
+        <h3>Count: {count}</h3>
         <button onClick={increment}>+</button>
-        <h2>Expensive Calculation : {calculation}</h2>
+        <h2>Expensive Calculation: {calculation}</h2>
       </div>
-    </>
+    </div>
   );
 };
 
